8aaf1527d630137656d0405f4b52d351b358b086
anika-gup <143746022+anika-gup@users.noreply.github.com>
2025-02-06 13:10:43 -0800

    Add files via upload

---
+++ node.cpp
@@ -0,0 +1,42 @@
+//node.cpp
+#include "node.h"
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+node::node() {
+  value = 0;
+  strcpy(label, "");
+  next = NULL;
+  accept = false;
+}
+node::~node() {
+  // i still have no idea what this does YAY !
+}
+void node::setNext(node* n) {
+  next = n;
+}
+int node::getValue() {
+  return value;
+}
+node* node::getNext() {
+  return next;
+}
+
+void node::setAccept(bool acceptState) {
+  accept = acceptState;
+}
+
+bool node::getAccept() {
+  return accept;
+}
+
+void node::setLabel(char* mylabel) {
+  strcpy(label, mylabel);
+  //label = mylabel;
+}
+
+char* node::getLabel() {
+  return label;
+}
